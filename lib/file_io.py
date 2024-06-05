865e570b3aa075d2a8e4836accf2df04cf39a0fe
princess <princessmumbi001@gmail.com>
2024-06-05 15:25:55 +0300

    fileio lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt",'w') as file :
+        file.write(f'{file_content}')
+    
 
 def append_file(file_name, append_content):
+    with open(f"{file_name}.txt",'a') as file :
+        file.write(f'{append_content}')
     pass
 
 def read_file(file_name):
+    with open(f"{file_name}.txt",'r') as file :
+        return file.read()
     pass
